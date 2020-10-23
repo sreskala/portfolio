25611df91648ae258d83de4b5a030bcac4bd1977
sreskala <sam.sagas@hotmail.com>
2020-10-23 10:44:47 -0400

    enhanced the parallax header for several screens

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -6,31 +6,41 @@ import sky from '../../assets/Background.png';
 import mountains from '../../assets/MiddleImage.png';
 import city from '../../assets/FrontImage.png';
 
-import {useLaxElement} from 'use-lax';
+import {useLax, useLaxElement} from 'use-lax';
 
 const HomePage = () => {
+
+    useLax();
 
     const ref = useLaxElement();
 
     return (
         <Fragment>
-        <div className="container">
+            <div className="container">
 
-            <div ref={ref} data-lax-translate-y="0 1, 400 -50" className="sky lax">
-                <img src={sky} alt="sky"/>
+               
+                    <h1
+                    className="title-header"
+                    //ref={ref}
+                    data-lax-scale="0 1, vh 0.2"
+                    data-lax-translate-y="0 0, vh 1200"
+                    data-lax-opacity="0 1, (vh*0.5) 0"
+                    >Sam's Header</h1>
+               
+
+                <div ref={ref} data-lax-translate-y="0 1, 400 -50" className="sky lax">
+                    <img src={sky} alt="sky"/>
+                </div>
+
+                <div ref={ref} data-lax-translate-y="0 1, 400 -200" className="mountains lax">
+                    <img src={mountains} alt="mountains"/>
+                </div>
+
+                <div ref={ref} data-lax-translate-y="0 1, 400 -340" className="city lax">
+                    <img src={city} alt="city"/>
+                </div>
+
             </div>
-
-            <div ref={ref} data-lax-translate-y="0 1, 400 -200" className="mountains lax">
-                <img src={mountains} alt="mountains"/>
-            </div>
-
-            <div ref={ref} data-lax-translate-y="0 1, 400 -340" className="city lax">
-                <img src={city} alt="city" />
-            </div>
-
-            
-
-        </div>
 
         </Fragment>
     )
